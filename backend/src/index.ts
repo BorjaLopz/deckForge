c852c37dd975320f1181fe37dcc68cc8de2ec97f
Borja López Díaz <lopzdiazborja@gmail.com>
2026-08-14 22:07:12 +0200

    Actualización: healthcheck con supabase

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import { Pool } from "pg";
 
 dotenv.config();
 
@@ -8,8 +9,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+})
+
 app.get("/api/ping", (req, res) => {
     res.json({ message: "pong desde Render", timestamp: Date.now() })
+})
+
+app.get("/api/db-check", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json({ status: "ok", dbTime: result.rows[0].now });
+    } catch (error) {
+        console.error("Error conectando a la base de datos: ", error);
+        res.status(500).json({ status: "error", message: "No se pudo conectar a la base de datos de supabase" });
+    }
 })
 
 const PORT = process.env.PORT || 3000;
