68733211bdc3887ee895502ac67022dcab909bb8
Borja López Díaz <lopzdiazborja@gmail.com>
2026-08-16 14:02:47 +0200

    corrección deploy

--- frontend/src/pages/ListadoCartasPage.tsx
+++ frontend/src/pages/ListadoCartasPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useSearchParams, useLocation, Link } from "react-router-dom";
+import { useSearchParams, useLocation } from "react-router-dom";
 import { buscarCartas } from "../services/cartasService";
 import CartaResumen from "../components/CartaResumen";
 
